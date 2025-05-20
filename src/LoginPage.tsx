91a9acc2bfc7eb1f55494ab1da9130afd4e3ed9b
Manonthemon <klimowicz.maciej@gmail.com>
2025-05-20 18:58:29 +0100

    Fixed input and button colors

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -22,51 +22,62 @@ function LoginPage({}: loginProps) {
     <div className="flex justify-center items-center min-h-screen ">
       <div className="bg-gradient-to-r from-gradient-start to-gradient-end p-8 rounded-lg shadow-2xl bg-opacity-80 backdrop-blur-md max-w-xs w-full ">
 
-        <form onSubmit={handleSubmit(onSubmit)}>
+       <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
 
           {/* USERNAME       */}
-          <div className="mb-4">
+          <div>
             <label
               htmlFor="username"
-              className="block mb-4 text-white font-semibold uppercase"
+              className="block mb-1 text-white font-semibold uppercase"
             >
               usuername
             </label>
             <Input
               type="text"
               id="username"
-              className="p-2 rounded-full border border-gray-300 w-full"
+              className="p-2 rounded-full border border-gray-300 w-full bg-white"
               {...register("username", { required: "Username is required." })}
             ></Input>
             {errors.username && (
-              <p className="text-red-500 text-sm mt-0.5">{errors.username.message as string}</p>
+                   <p className={`min-h-[1.25rem] text-red-500 text-sm mt-0.5 ${errors.username ? 'visible' : 'invisible'}`}>
+                {errors.username.message as string}</p>
             )}
           </div>
 
           {/* PASSWORD */}
-          <div className="mb-6">
+          <div>
             <label
               htmlFor="password"
-              className="block mb-4 text-white font-semibold uppercase"
+              className="block mb-1 text-white font-semibold uppercase"
             >
               password
             </label>
             <Input
               type="password"
               id="password"
-              className="p-2 rounded-full border border-gray-300 w-full"
+              className="p-2 rounded-full border border-gray-300 w-full bg-white"
                {...register("password", { required: "Password is required." })}
             ></Input>
               {errors.password && (
-              <p className="text-red-500 text-sm mt-0.5">{errors.password.message as string}</p>
+                   <p className={`min-h-[1.25rem] text-red-500 text-sm mt-0.5 ${errors.username ? 'visible' : 'invisible'}`}>
+                {errors.password.message as string}</p>
             )}
           </div>
 
           {/* BUTTON */}
           <div className="flex justify-end">
             <Button
+            
               type="submit"
-              className="box-border px-4 py-1 bg-poolgoods-button-blue text-white rounded-full font-semibold shadow-xl hover:outline hover:outline-white hover:outline-solid hover:outline-1"
+              className="
+      box-border px-4 py-1 
+      bg-poolgoods-button-blue text-white 
+      rounded-full font-semibold shadow-xl 
+      border border-transparent             
+      hover:border-white             
+      hover:border-1                       
+      hover:bg-poolgoods-button-blue       
+    "
             >
               Login
             </Button>
